c8d9890d24aeb34257350055ad6c585a0e758358
sarahbear <nuriyejiao@gmail.com>
2015-01-06 20:07:39 +0800

    伪造数据 & 注释list页

--- app.js
+++ app.js
@@ -8,28 +8,82 @@ app.listen(port)
 
 console.log('imooc started on port ' + port)
 
-//index page
+
 app.get('/',function(req,res){
 	res.render('index',{
-		title:'imooc index'
+		title:'影院热度播报',
+		movies:[{
+			title:'机械战警',
+			_id:1,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title:'机械战警',
+			_id:2,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title:'机械战警',
+			_id:3,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title:'机械战警',
+			_id:4,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+			title:'机械战警',
+			_id:5,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		}
+		,{
+			title:'机械战警',
+			_id:6,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		}]
 	})
 })
 
-//detail page
 app.get('/movie/:id',function(req,res){
 	res.render('detail',{
-		title:'imooc detail'
+		title:'imooc 详情页',
+		movie:{
+			doctor:'何塞',
+			country:'美国',
+			title:'机械战警',
+			year:2014,
+			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language:'英语',
+			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
+			summary:'2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。 '
+		}
 	})
 })
-//admin page
+
 app.get('/admin/movie',function(req,res){
 	res.render('admin',{
-		title:'imooc 后台录入'
+		title:'imooc 后台录入页面',
+		movie:{
+			doctor:'',
+			country:'',
+			title:'',
+			year:'',
+			poster:'',
+			language:'',
+			flash:'',
+			summary:''
+		}
 	})
 })
-//list page
+
 app.get('/admin/list',function(req,res){
 	res.render('list',{
-		title:'imooc 列表'
+		title:'imooc 列表页',
+		movies:[{
+			title:'机械战警',
+			_id:1,
+			doctor:'何塞',
+			country:'美国',
+			year:'2014',
+			language:'英语',
+			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
+		}]
 	})
 })
