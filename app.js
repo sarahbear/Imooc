b355ef07f22b7bc893aea45428f4882768da2bf3
sarahbear <nuriyejiao@gmail.com>
2015-01-09 10:45:36 +0800

    add underscore, add mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 var mongoose = require('mongoose')
+var _ = require('underscore')
 var Movie = require('./models/movie')
 var bodyParser = require('body-parser')
 var port = process.env.PORT || 3000
@@ -12,6 +13,7 @@ app.set('views','./views/pages')
 app.set('view engine','jade')
 app.use(bodyParser())
 app.use(express.static(path.join(__dirname,'bower_components')))
+app.locals.moment = require('moment')
 app.listen(port)
 
 console.log('imooc started on port ' + port)
@@ -30,18 +32,13 @@ app.get('/',function(req,res){
 })
 
 app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-		title:'imooc 详情页',
-		movie:{
-			doctor:'何塞',
-			country:'美国',
-			title:'机械战警',
-			year:2014,
-			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-			language:'英语',
-			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
-			summary:'2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。 '
-		}
+	var id = req.params.id
+
+	Movie.findById(id,function(err,movie){
+		res.render('detail',{
+			title:'imooc ' + movie.title,
+			movie:movie
+		})	
 	})
 })
 
@@ -61,17 +58,58 @@ app.get('/admin/movie',function(req,res){
 	})
 })
 
+//admin post movie
+app.post('/admin/movie/new',function(req,res){
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+
+	if(id!=='undefined'){
+		Movie.findById(id, function(err,movie){
+			if(err) {
+				console.log(err)
+			}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+
+				res.redirect('/movie' + movie._id)
+			})
+		})
+	}
+	else{
+		_movie = new Movie({
+			doctor:movieObj.doctor,
+			title:movieObj.title,
+			country:movieObj.country,
+			language:movieObj.language,
+			year:movieObj.year,
+			poster:movieObj.poster,
+			summary:movieObj.summary,
+			flash:movieObj.flash,
+		})
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err)
+		    }
+
+		    res.redirect('/movie/'+_movie.id)
+		})
+	}
+})
+
+
 app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'imooc 列表页',
-		movies:[{
-			title:'机械战警',
-			_id:1,
-			doctor:'何塞',
-			country:'美国',
-			year:'2014',
-			language:'英语',
-			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
-		}]
+	Movie.fetch(function(err, movies){
+		if(err) {
+			console.log(err)
+		}
+		res.render('list',{
+			title:'imooc 列表页',
+			movies:movies
+		})
 	})
 })
